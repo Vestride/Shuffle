91535600de3f011a9eb72e0b555091250def55d8
Glen Cheney <cheney.glen@gmail.com>
2013-05-21 00:49:19 -0700

    Gettin stylish

--- js/demos/homepage.js
+++ js/demos/homepage.js
@@ -1,75 +1,105 @@
-$(document).ready(function() {
+var DEMO = (function($, window) {
+  'use strict';
 
-        // Set up button clicks
-        $('.filter-options li').on('click', function() {
-            var $this = $(this),
-                $grid = $('#grid');
+  var $grid = $('#grid'),
+      $filterOptions = $('.filter-options'),
 
-            // Hide current label, show current label in title
-            $('.filter-options .active').removeClass('active');
-            $this.addClass('active');
+  init = function() {
+    setupFilters();
+    setupSorting();
+    setupSearching();
 
-            // Filter elements
-            $grid.shuffle($this.data('group'));
-        });
+    // instantiate the plugin
+    $('#grid').shuffle();
 
-        // Sorting options
-        $('.sort-options li').on('click', function() {
-            var $this = $(this),
-                $grid = $('#grid'),
-                sort = $this.data('sort'),
-                opts = {};
+    // Destroy it! o_O
+    // $('#grid').shuffle('destroy');
 
-            // Hide current label, show current label in title
-            $('.sort-options .active').removeClass('active');
-            $this.addClass('active');
-
-            // We're given the element wrapped in jQuery
-            if (sort === 'date-created') {
-                opts = {
-                    by: function($el) {
-                        return $el.data('date-created');
-                    }
-                }
-            } else if (sort === 'title') {
-                opts = {
-                    by: function($el) {
-                        return $el.data('title').toLowerCase();
-                    }
-                };
-            }
-
-            // Filter elements
-            $grid.shuffle('sort', opts);
-        });
-
-        // Advanced filtering
-        $('.filter .search').on('keyup change', function() {
-            var val = this.value.toLowerCase();
-            $('#grid').shuffle(function($el, shuffle) {
-
-                // Only search elements in the current group
-                if (shuffle.group !== 'all' && $.inArray(shuffle.group, $el.data('groups')) === -1) {
-                    return false;
-                }
-
-                var text = $.trim($el.text()).toLowerCase();
-                return text.indexOf(val) != -1;
-            });
-        });
-
-        // instantiate the plugin
-        $('#grid').shuffle();
-
-        // Destroy it! o_O
-        // $('#grid').shuffle('destroy');
-
-
-        // You can subscribe to custom events: shrink, shrunk, filter, filtered, and sorted
-        $('#grid').on('shrink.shuffle shrunk.shuffle filter.shuffle filtered.shuffle sorted.shuffle layout.shuffle', function(evt, shuffle) {
-            if ( window.console ) {
-                console.log(evt.type, shuffle, this);
-            }
-        });
-
+    // You can subscribe to custom events: shrink, shrunk, filter, filtered, and sorted
+    $('#grid').on('shrink.shuffle shrunk.shuffle filter.shuffle filtered.shuffle sorted.shuffle layout.shuffle', function(evt, shuffle) {
+      if ( window.console ) {
+        console.log(evt.type, shuffle, this);
+      }
     });
+  },
+
+  // Set up button clicks
+  setupFilters = function() {
+    var $btns = $filterOptions.children();
+    $btns.on('click', function() {
+      var $this = $(this),
+          isActive = $this.hasClass( 'active' ),
+          group = isActive ? 'all' : $this.data('group');
+
+      // Hide current label, show current label in title
+      if ( !isActive ) {
+        $('.filter-options .active').removeClass('active');
+      }
+
+      $this.toggleClass('active');
+
+      // Filter elements
+      $grid.shuffle( group );
+    });
+
+    $btns = null;
+  },
+
+  setupSorting = function() {
+    // Sorting options
+    $('.sort-options button').on('click', function() {
+      var $this = $(this),
+          sort = $this.data('sort'),
+          opts = {};
+
+      // Hide current label, show current label in title
+      $('.sort-options .active').removeClass('active');
+      $this.addClass('active');
+
+      // We're given the element wrapped in jQuery
+      if (sort === 'date-created') {
+        opts = {
+          by: function($el) {
+            return $el.data('date-created');
+          }
+        }
+      } else if (sort === 'title') {
+        opts = {
+          by: function($el) {
+            return $el.data('title').toLowerCase();
+          }
+        };
+      }
+
+      // Filter elements
+      $grid.shuffle('sort', opts);
+    });
+  },
+
+  setupSearching = function() {
+    // Advanced filtering
+    $('.filter .search').on('keyup change', function() {
+      var val = this.value.toLowerCase();
+      $grid.shuffle(function($el, shuffle) {
+
+        // Only search elements in the current group
+        if (shuffle.group !== 'all' && $.inArray(shuffle.group, $el.data('groups')) === -1) {
+          return false;
+        }
+
+        var text = $.trim($el.text()).toLowerCase();
+        return text.indexOf(val) != -1;
+      });
+    });
+  };
+
+  return {
+    init: init
+  };
+}(jQuery, window));
+
+
+
+$(document).ready(function() {
+  DEMO.init();
+});
