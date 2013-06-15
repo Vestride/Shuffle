410762f8e8806c74b72125925751e3d8f2b8a19a
Glen Cheney <cheney.glen@gmail.com>
2013-06-15 01:29:28 -0700

    Ordering getters and setters so only one style recalculation and layout happens on init. Added $.throttle / $.debounce and more function comments.

--- js/jquery.shuffle.js
+++ js/jquery.shuffle.js
@@ -6,9 +6,23 @@
  */
 ;window.Modernizr=function(a,b,c){function z(a){j.cssText=a}function A(a,b){return z(m.join(a+";")+(b||""))}function B(a,b){return typeof a===b}function C(a,b){return!!~(""+a).indexOf(b)}function D(a,b){for(var d in a){var e=a[d];if(!C(e,"-")&&j[e]!==c)return b=="pfx"?e:!0}return!1}function E(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:B(f,"function")?f.bind(d||b):f}return!1}function F(a,b,c){var d=a.charAt(0).toUpperCase()+a.slice(1),e=(a+" "+o.join(d+" ")+d).split(" ");return B(b,"string")||B(b,"undefined")?D(e,b):(e=(a+" "+p.join(d+" ")+d).split(" "),E(e,b,c))}var d="2.6.2",e={},f=!0,g=b.documentElement,h="modernizr",i=b.createElement(h),j=i.style,k,l={}.toString,m=" -webkit- -moz- -o- -ms- ".split(" "),n="Webkit Moz O ms",o=n.split(" "),p=n.toLowerCase().split(" "),q={},r={},s={},t=[],u=t.slice,v,w=function(a,c,d,e){var f,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:h+(d+1),l.appendChild(j);return f=["&#173;",'<style id="s',h,'">',a,"</style>"].join(""),l.id=h,(m?l:n).innerHTML+=f,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=g.style.overflow,g.style.overflow="hidden",g.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),g.style.overflow=k),!!i},x={}.hasOwnProperty,y;!B(x,"undefined")&&!B(x.call,"undefined")?y=function(a,b){return x.call(a,b)}:y=function(a,b){return b in a&&B(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=u.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(u.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(u.call(arguments)))};return e}),q.csstransforms=function(){return!!F("transform")},q.csstransforms3d=function(){var a=!!F("perspective");return a&&"webkitPerspective"in g.style&&w("@media (transform-3d),(-webkit-transform-3d){#modernizr{left:9px;position:absolute;height:3px;}}",function(b,c){a=b.offsetLeft===9&&b.offsetHeight===3}),a},q.csstransitions=function(){return F("transition")};for(var G in q)y(q,G)&&(v=G.toLowerCase(),e[v]=q[G](),t.push((e[v]?"":"no-")+v));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)y(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof f!="undefined"&&f&&(g.className+=" "+(b?"":"no-")+a),e[a]=b}return e},z(""),i=k=null,function(a,b){function k(a,b){var c=a.createElement("p"),d=a.getElementsByTagName("head")[0]||a.documentElement;return c.innerHTML="x<style>"+b+"</style>",d.insertBefore(c.lastChild,d.firstChild)}function l(){var a=r.elements;return typeof a=="string"?a.split(" "):a}function m(a){var b=i[a[g]];return b||(b={},h++,a[g]=h,i[h]=b),b}function n(a,c,f){c||(c=b);if(j)return c.createElement(a);f||(f=m(c));var g;return f.cache[a]?g=f.cache[a].cloneNode():e.test(a)?g=(f.cache[a]=f.createElem(a)).cloneNode():g=f.createElem(a),g.canHaveChildren&&!d.test(a)?f.frag.appendChild(g):g}function o(a,c){a||(a=b);if(j)return a.createDocumentFragment();c=c||m(a);var d=c.frag.cloneNode(),e=0,f=l(),g=f.length;for(;e<g;e++)d.createElement(f[e]);return d}function p(a,b){b.cache||(b.cache={},b.createElem=a.createElement,b.createFrag=a.createDocumentFragment,b.frag=b.createFrag()),a.createElement=function(c){return r.shivMethods?n(c,a,b):b.createElem(c)},a.createDocumentFragment=Function("h,f","return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&("+l().join().replace(/\w+/g,function(a){return b.createElem(a),b.frag.createElement(a),'c("'+a+'")'})+");return n}")(r,b.frag)}function q(a){a||(a=b);var c=m(a);return r.shivCSS&&!f&&!c.hasCSS&&(c.hasCSS=!!k(a,"article,aside,figcaption,figure,footer,header,hgroup,nav,section{display:block}mark{background:#FF0;color:#000}")),j||p(a,c),a}var c=a.html5||{},d=/^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i,e=/^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i,f,g="_html5shiv",h=0,i={},j;(function(){try{var a=b.createElement("a");a.innerHTML="<xyz></xyz>",f="hidden"in a,j=a.childNodes.length==1||function(){b.createElement("a");var a=b.createDocumentFragment();return typeof a.cloneNode=="undefined"||typeof a.createDocumentFragment=="undefined"||typeof a.createElement=="undefined"}()}catch(c){f=!0,j=!0}})();var r={elements:c.elements||"abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video",shivCSS:c.shivCSS!==!1,supportsUnknownElements:j,shivMethods:c.shivMethods!==!1,type:"default",shivDocument:q,createElement:n,createDocumentFragment:o};a.html5=r,q(b)}(this,b),e._version=d,e._prefixes=m,e._domPrefixes=p,e._cssomPrefixes=o,e.testProp=function(a){return D([a])},e.testAllProps=F,e.testStyles=w,e.prefixed=function(a,b,c){return b?F(a,b,c):F(a,"pfx")},g.className=g.className.replace(/(^|\s)no-js(\s|$)/,"$1$2")+(f?" js "+t.join(" "):""),e}(this,this.document),function(a,b,c){function d(a){return"[object Function]"==o.call(a)}function e(a){return"string"==typeof a}function f(){}function g(a){return!a||"loaded"==a||"complete"==a||"uninitialized"==a}function h(){var a=p.shift();q=1,a?a.t?m(function(){("c"==a.t?B.injectCss:B.injectJs)(a.s,0,a.a,a.x,a.e,1)},0):(a(),h()):q=0}function i(a,c,d,e,f,i,j){function k(b){if(!o&&g(l.readyState)&&(u.r=o=1,!q&&h(),l.onload=l.onreadystatechange=null,b)){"img"!=a&&m(function(){t.removeChild(l)},50);for(var d in y[c])y[c].hasOwnProperty(d)&&y[c][d].onload()}}var j=j||B.errorTimeout,l=b.createElement(a),o=0,r=0,u={t:d,s:c,e:f,a:i,x:j};1===y[c]&&(r=1,y[c]=[]),"object"==a?l.data=c:(l.src=c,l.type=a),l.width=l.height="0",l.onerror=l.onload=l.onreadystatechange=function(){k.call(this,r)},p.splice(e,0,u),"img"!=a&&(r||2===y[c]?(t.insertBefore(l,s?null:n),m(k,j)):y[c].push(l))}function j(a,b,c,d,f){return q=0,b=b||"j",e(a)?i("c"==b?v:u,a,b,this.i++,c,d,f):(p.splice(this.i++,0,a),1==p.length&&h()),this}function k(){var a=B;return a.loader={load:j,i:0},a}var l=b.documentElement,m=a.setTimeout,n=b.getElementsByTagName("script")[0],o={}.toString,p=[],q=0,r="MozAppearance"in l.style,s=r&&!!b.createRange().compareNode,t=s?l:n.parentNode,l=a.opera&&"[object Opera]"==o.call(a.opera),l=!!b.attachEvent&&!l,u=r?"object":l?"script":"img",v=l?"script":u,w=Array.isArray||function(a){return"[object Array]"==o.call(a)},x=[],y={},z={timeout:function(a,b){return b.length&&(a.timeout=b[0]),a}},A,B;B=function(a){function b(a){var a=a.split("!"),b=x.length,c=a.pop(),d=a.length,c={url:c,origUrl:c,prefixes:a},e,f,g;for(f=0;f<d;f++)g=a[f].split("="),(e=z[g.shift()])&&(c=e(c,g));for(f=0;f<b;f++)c=x[f](c);return c}function g(a,e,f,g,h){var i=b(a),j=i.autoCallback;i.url.split(".").pop().split("?").shift(),i.bypass||(e&&(e=d(e)?e:e[a]||e[g]||e[a.split("/").pop().split("?")[0]]),i.instead?i.instead(a,e,f,g,h):(y[i.url]?i.noexec=!0:y[i.url]=1,f.load(i.url,i.forceCSS||!i.forceJS&&"css"==i.url.split(".").pop().split("?").shift()?"c":c,i.noexec,i.attrs,i.timeout),(d(e)||d(j))&&f.load(function(){k(),e&&e(i.origUrl,h,g),j&&j(i.origUrl,h,g),y[i.url]=2})))}function h(a,b){function c(a,c){if(a){if(e(a))c||(j=function(){var a=[].slice.call(arguments);k.apply(this,a),l()}),g(a,j,b,0,h);else if(Object(a)===a)for(n in m=function(){var b=0,c;for(c in a)a.hasOwnProperty(c)&&b++;return b}(),a)a.hasOwnProperty(n)&&(!c&&!--m&&(d(j)?j=function(){var a=[].slice.call(arguments);k.apply(this,a),l()}:j[n]=function(a){return function(){var b=[].slice.call(arguments);a&&a.apply(this,b),l()}}(k[n])),g(a[n],j,b,n,h))}else!c&&l()}var h=!!a.test,i=a.load||a.both,j=a.callback||f,k=j,l=a.complete||f,m,n;c(h?a.yep:a.nope,!!i),i&&c(i)}var i,j,l=this.yepnope.loader;if(e(a))g(a,0,l,0);else if(w(a))for(i=0;i<a.length;i++)j=a[i],e(j)?g(j,0,l,0):w(j)?B(j):Object(j)===j&&h(j,l);else Object(a)===a&&h(a,l)},B.addPrefix=function(a,b){z[a]=b},B.addFilter=function(a){x.push(a)},B.errorTimeout=1e4,null==b.readyState&&b.addEventListener&&(b.readyState="loading",b.addEventListener("DOMContentLoaded",A=function(){b.removeEventListener("DOMContentLoaded",A,0),b.readyState="complete"},0)),a.yepnope=k(),a.yepnope.executeStack=h,a.yepnope.injectJs=function(a,c,d,e,i,j){var k=b.createElement("script"),l,o,e=e||B.errorTimeout;k.src=a;for(o in d)k.setAttribute(o,d[o]);c=j?h:c||f,k.onreadystatechange=k.onload=function(){!l&&g(k.readyState)&&(l=1,c(),k.onload=k.onreadystatechange=null)},m(function(){l||(l=1,c(1))},e),i?k.onload():n.parentNode.insertBefore(k,n)},a.yepnope.injectCss=function(a,c,d,e,g,i){var e=b.createElement("link"),j,c=i?h:c||f;e.href=a,e.rel="stylesheet",e.type="text/css";for(j in d)e.setAttribute(j,d[j]);g||(n.parentNode.insertBefore(e,n),m(c,0))}}(this,document),Modernizr.load=function(){yepnope.apply(window,[].slice.call(arguments,0))};
 
+
+// Shuffle Doesn't require this shuffle/debounce plugin, but it works better with it.
+
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
 /*!
  * jQuery Shuffle Plugin
- * Uses CSS Transforms to filter down a grid of items. Requires jQuery 1.9+.
+ * Uses CSS Transforms to filter down a grid of items.
+ * Dependencies: jQuery 1.9+, Modernizr 2.6.2. Optionally throttle/debounce by Ben Alman
  * Inspired by Isotope http://isotope.metafizzy.co/
  * Licensed under the MIT license.
  * @author Glen Cheney (http://glencheney.com)
@@ -110,18 +124,16 @@ $.fn.sorted.randomize = function( array ) {
 var id = 0;
 
 var Shuffle = function( $container, options ) {
-    console.time('shuffle initialized');
     var self = this;
     $.extend( self, Shuffle.options, options, Shuffle.settings );
 
-    self.$container = $container.addClass('shuffle');
+    self.$container = $container;
     self.$window = $(window);
     self.unique = 'shuffle_' + id++;
 
     self.fire('loading');
     self._init();
     self.fire('done');
-    console.timeEnd('shuffle initialized');
 };
 
 Shuffle.prototype = {
@@ -129,22 +141,32 @@ Shuffle.prototype = {
     constructor: Shuffle,
 
     _init : function() {
-        console.timeStamp('Get container CSS');
         var self = this,
-            containerCSS = self.$container.css(['paddingLeft', 'paddingRight', 'position', 'width']),
+            containerCSS,
             resizeFunction = $.proxy( self._onResize, self ),
             debouncedResize = self.throttle ? self.throttle( self.throttleTime, resizeFunction ) : resizeFunction;
 
+        // Save variables needed later then add some classes
         self._setVars();
+
+        // Zero out all columns
+        self._resetCols();
+
+        // Add classes and invalidate styles
+        self._addClasses();
+
+        // Set up css for transitions and set initial css for each item
+        self.$container[0].style[ self.transitionName ] = 'height ' + self.speed + 'ms ' + self.easing;
+        self._initItems();
 
         // Bind resize events (http://stackoverflow.com/questions/1852751/window-resize-event-firing-in-internet-explorer)
         self.$window.on('resize.shuffle.' + self.unique, debouncedResize);
 
-        // Set up css for transitions
-        self.$container[0].style[ self.transitionName ] = 'height ' + self.speed + 'ms ' + self.easing;
-        self._initItems();
+        // Get container css all in one request
+        containerCSS = self.$container.css(['paddingLeft', 'paddingRight', 'position', 'width']);
 
-        // Position cannot be static
+        // Position cannot be static.
+        // This will cause an extra style layout if it has to be set and the sizer element is used.
         if ( containerCSS.position === 'static' ) {
             self.$container.css('position', 'relative');
         }
@@ -154,9 +176,6 @@ Shuffle.prototype = {
             left: parseInt( containerCSS.paddingLeft, 10 ) || 0,
             top: parseInt( containerCSS.paddingTop, 10 ) || 0
         };
-
-        // Zero out all columns
-        self._resetCols();
 
         // We already got the container's width above, no need to cause another reflow getting it again...
         // Calculate the number of columns there will be
@@ -169,16 +188,25 @@ Shuffle.prototype = {
         // so the user doesn't see the first layout. Set them now that the first layout is done.
         if ( self.supported ) {
             setTimeout(function() {
-                console.timeStamp('SET TRANSITIONS');
                 self._setTransitions();
             }, 0);
         }
     },
 
+    // Will invalidate styles
+    _addClasses : function() {
+        var self = this;
+
+        self.$container.addClass('shuffle');
+        self.$items.addClass('shuffle-item filtered');
+
+        return self;
+    },
+
     _setVars : function() {
         var self = this;
 
-        self.$items = self._getItems().addClass('shuffle-item filtered');
+        self.$items = self._getItems();
         self.transitionName = self.prefixed('transition'),
         self.transitionDelayName = self.prefixed('transitionDelay');
         self.transitionDuration = self.prefixed('transitionDuration');
@@ -215,6 +243,8 @@ Shuffle.prototype = {
             self.useSizer = true;
             self.sizer = self.$sizer[0];
         }
+
+        return self;
     },
 
     _filter : function( category, $collection ) {
@@ -354,16 +384,17 @@ Shuffle.prototype = {
     },
 
     getPreciseDimension : function( element, style ) {
-      var dimension;
-      if ( window.getComputedStyle ) {
-        dimension = window.getComputedStyle( element, null )[ style ];
-      } else {
-        dimension = $( element ).css( dimension );
-      }
-      return parseFloat( dimension );
+        var dimension;
+        if ( window.getComputedStyle ) {
+            dimension = window.getComputedStyle( element, null )[ style ];
+        } else {
+            dimension = $( element ).css( dimension );
+        }
+        return parseFloat( dimension );
     },
 
     // Calculate number of columns
+    // Gets css if using sizer element
     _setColumns : function( theContainerWidth ) {
         var self = this,
             containerWidth = theContainerWidth || self.$container.width(),
@@ -390,7 +421,6 @@ Shuffle.prototype = {
 
         self.colWidth += gutter;
 
-        // Was flooring 4.999999999999999 to 4 :(
         calculatedColumns = (containerWidth + gutter) / self.colWidth;
         // Widths given from getComputedStyle are not precise enough...
         if ( Math.ceil(calculatedColumns) - calculatedColumns < 0.01 ) {
@@ -574,18 +604,13 @@ Shuffle.prototype = {
         self.shrinkTransitionEnded = false;
         $concealed.each(function() {
             var $this = $(this),
-                data = $this.data(),
-                x = parseInt( data.x, 10 ),
-                y = parseInt( data.y, 10 );
-
-            if (!x) { x = 0; }
-            if (!y) { y = 0; }
+                data = $this.data();
 
             transitionObj = {
                 from: 'shrink',
                 $this: $this,
-                x: x,
-                y: y,
+                x: data.x,
+                y: data.y,
                 scale : 0.001,
                 opacity: 0,
                 callback: callback
@@ -777,10 +802,13 @@ Shuffle.prototype = {
         }
 
         $newItems.addClass('shuffle-item');
-        self._initItems( undefined, $newItems );
+        self._initItems( $newItems );
         self.$items = self._getItems();
-        $newItems.not($passed).css('opacity', 0);
 
+        // Hide all items
+        $newItems.css('opacity', 0);
+
+        // Get ones that passed the current filter
         $passed = self._filter( undefined, $newItems );
         passed = $passed.get();
 
@@ -921,10 +949,17 @@ Shuffle.prototype = {
         this._addItems( $newItems, animateIn, isSequential );
     },
 
+    /**
+     * Disables shuffle from updating dimensions and layout on resize
+     */
     disable : function() {
         this.enabled = false;
     },
 
+    /**
+     * Enables shuffle again
+     * @param  {Boolean} isUpdateLayout if undefined, shuffle will update columns and gutters
+     */
     enable : function( isUpdateLayout ) {
         this.enabled = true;
         if ( isUpdateLayout !== false ) {
@@ -932,6 +967,11 @@ Shuffle.prototype = {
         }
     },
 
+    /**
+     * Remove 1 or more shuffle items
+     * @param  {jQuery} $collection a jQuery object containing one or more element in shuffle
+     * @return {Shuffle}             the shuffle object
+     */
     remove : function( $collection ) {
 
         // If this isn't a jquery object, exit
@@ -956,6 +996,7 @@ Shuffle.prototype = {
 
         self.shrink( $collection, remove );
         self._processStyleQueue();
+        return self;
     },
 
     destroy: function() {
@@ -985,7 +1026,7 @@ Shuffle.options = {
     showInitialTransition : false, // If set to false, the shuffle-items will only have a transition applied to them after the first layout
     delimeter : null, // if your group is not json, and is comma delimeted, you could set delimeter to ','
     buffer: 0, // useful for percentage based heights when they might not always be exactly the same (in pixels)
-    throttle: $.debounce || null,
+    throttle: $.throttle || null,
     throttleTime: 300,
     sequentialFadeDelay: 150,
     supported: Modernizr.csstransforms && Modernizr.csstransitions // supports transitions and transforms
