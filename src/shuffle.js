78d24cc8ee308e7dd188599eb28176d65bb383d4
Glen Cheney <cheney.glen@gmail.com>
2014-07-09 21:47:50 -0700

    Update docs and options descriptions.

--- src/shuffle.js
+++ src/shuffle.js
@@ -150,7 +150,6 @@ var Shuffle = function( element, options ) {
 Shuffle.EventType = {
   LOADING: 'loading',
   DONE: 'done',
-  SORTED: 'sorted',
   LAYOUT: 'layout',
   REMOVED: 'removed'
 };
@@ -1289,19 +1288,19 @@ Shuffle.prototype.destroy = function() {
 // Overrideable options
 Shuffle.options = {
   group: ALL_ITEMS, // Initial filter group.
-  speed: 250, // Transition/animation speed (milliseconds)
-  easing: 'ease-out', // css easing function to use
-  itemSelector: '', // e.g. '.picture-item'
-  sizer: null, // sizer element. Use an element to determine the size of columns and gutters.
-  gutterWidth: 0, // a static number or function that tells the plugin how wide the gutters between columns are (in pixels)
-  columnWidth: 0, // a static number or function that returns a number which tells the plugin how wide the columns are (in pixels)
-  delimeter: null, // if your group is not json, and is comma delimeted, you could set delimeter to ','
-  buffer: 0, // useful for percentage based heights when they might not always be exactly the same (in pixels)
-  initialSort: null, // Shuffle can be initialized with a sort object. It is the same object given to the sort method
-  throttle: throttle, // By default, shuffle will try to throttle the resize event. This option will change the method it uses
-  throttleTime: 300, // How often shuffle can be called on resize (in milliseconds)
-  sequentialFadeDelay: 150, // Delay between each item that fades in when adding items
-  supported: CAN_TRANSITION_TRANSFORMS // supports transitions and transforms
+  speed: 250, // Transition/animation speed (milliseconds).
+  easing: 'ease-out', // CSS easing function to use.
+  itemSelector: '', // e.g. '.picture-item'.
+  sizer: null, // Sizer element. Use an element to determine the size of columns and gutters.
+  gutterWidth: 0, // A static number or function that tells the plugin how wide the gutters between columns are (in pixels).
+  columnWidth: 0, // A static number or function that returns a number which tells the plugin how wide the columns are (in pixels).
+  delimeter: null, // If your group is not json, and is comma delimeted, you could set delimeter to ','.
+  buffer: 0, // Useful for percentage based heights when they might not always be exactly the same (in pixels).
+  initialSort: null, // Shuffle can be initialized with a sort object. It is the same object given to the sort method.
+  throttle: throttle, // By default, shuffle will throttle resize events. This can be changed or removed.
+  throttleTime: 300, // How often shuffle can be called on resize (in milliseconds).
+  sequentialFadeDelay: 150, // Delay between each item that fades in when adding items.
+  supported: CAN_TRANSITION_TRANSFORMS // Whether to use transforms or absolute positioning.
 };
 
 
