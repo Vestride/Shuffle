eef5c66b31ee6b5d4468e5bf9233250e9d6a5591
Glen Cheney <cheney.glen@gmail.com>
2016-03-30 23:14:36 -0700

    Update images demo.

--- js/demos/images.js
+++ js/demos/images.js
@@ -1,54 +1,6 @@
+var Shuffle = window.shuffle;
 
-// For this demo, shuffle won't be initialized until
-// all the images have finished loading.
-
-// Another approach would be to initialize shuffle on document
-// ready and as imagesLoaded reports back progress, call shuffle.layout()
-
-// imagesLoaded: https://github.com/desandro/imagesloaded
-
-var ImageDemo = (function( $, imagesLoaded ) {
-
-  var $shuffle = $('.shuffle--images'),
-      $imgs = $shuffle.find('img'),
-      $loader = $('#loader'),
-      sizer = document.getElementById('js-sizer'),
-      imgLoad,
-
-  init = function() {
-
-    // Create a new imagesLoaded instance
-    imgLoad = new imagesLoaded( $imgs.get() );
-
-    // Listen for when all images are done
-    // will be executed even if some images fail
-    imgLoad.on( 'always', onAllImagesFinished );
-  },
-
-  onAllImagesFinished = function( instance ) {
-
-    if ( window.console && window.console.log ) {
-      console.log( instance );
-    }
-
-    // Hide loader
-    $loader.addClass('hidden');
-
-    // Adds visibility: visible;
-    $shuffle.addClass('images-loaded');
-
-    // Initialize shuffle
-    $shuffle.shuffle({
-      sizer: sizer,
-      itemSelector: '.js-item'
-    });
-  };
-
-  return {
-    init: init
-  };
-}( jQuery, window.imagesLoaded ));
-
-$(document).ready(function() {
-  ImageDemo.init();
+window.myShuffle = new Shuffle(document.querySelector('.my-grid-with-images'), {
+  itemSelector: '.js-item',
+  sizer: '.shuffle__sizer',
 });
